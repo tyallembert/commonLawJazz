07c2fe62aa896fd7ac004f456bb3bdd15227c2f2
Ty Allembert <tyallembert@gmail.com>
2023-08-27 21:28:26 -0400

    configured to work with AWS s3 bucket

--- src/songData.js
+++ src/songData.js
@@ -1,20 +1,19 @@
 
-import song1 from "./audio/MIX_01.wav";
 
 export const songsData = [
     {
       title: 'Song 1',
       artist: 'Author 1',
-      src: song1
+      src: "https://commonlawjazz.s3.amazonaws.com/MIX_01.wav"
     },
     {
         title: 'Song 2',
         artist: 'Author 2',
-        src: song1
+        src: "https://commonlawjazz.s3.amazonaws.com/MIX_01.wav"
       },
       {
         title: 'Song 3',
         artist: 'Author 3',
-        src: song1
+        src: "https://commonlawjazz.s3.amazonaws.com/MIX_01.wav"
       }
   ];
